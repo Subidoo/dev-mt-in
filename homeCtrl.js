ccdfcd90f37c53a25e2bb854ac48d4c9d09bf4c6
Kyle Prestgard <ktprestgard@gmail.com>
2016-02-25 19:32:09 -0700

    finishing project

--- homeCtrl.js
+++ homeCtrl.js
@@ -1,9 +1,7 @@
 angular.module("devMtIn")
-.controller("homeCtrl", function($scope, profileService) {
+.controller("homeCtrl", function($scope, profileService, friendService) {
 
 	$scope.editing = false;
-
-	$scope.myProfile = profileService.checkForProfile();
 
  	$scope.sortOptions = [{
     display: 'Ascending'
@@ -14,33 +12,59 @@ angular.module("devMtIn")
   , value: true
   }
 ];
-  	$scope.saveProfile = function(profile) {
-		profileService.saveProfile(profile);
-		$scope.editing = false;
-	}
-	$scope.deleteProfile = function() {
-  		profileService.deleteProfile()
-  		.then(function(deletedProfile) {
-   			 localStorage.removeItem('profileId');
-    		 $scope.myProfile = {};
-  			})
-  		.catch(function(err) {
-    		console.error(err);
- 		 });
-		}
 
-	$scope.checkForProfile = function() {
-  	var profileId = JSON.parse(localStorage.getItem('profileId'));
+$scope.checkForProfile = function() {
+  var profileId = JSON.parse(localStorage.getItem('profileId'));
 
-  	if (profileId) {
-    	profileService.checkForProfile(profileId.profileId)
-    	.then(function(profile) {
-      	$scope.myProfile = profile.data;
-    	})
+  if (profileId) {
+    profileService.checkForProfile(profileId.profileId)
+    .then(function(profile) {
+      $scope.myProfile = profile.data;
+      friendService.findFriendsFriends(profile.data);
+    })
     .catch(function(err) {
       console.error(err);
     });
   }
 }
-$scope.checkForProfile();
+
+	$scope.checkForProfile();
+
+  	$scope.saveProfile = function(profile) {
+		profileService.saveProfile(profile);
+		$scope.editing = false;
+	}
+	
+	$scope.deleteProfile = function(profile) {
+  		profileService.deleteProfile()
+  		.then(function(deletedProfile) {
+    		localStorage.removeItem('profileId');
+    		$scope.myProfile = {};
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+}
+
+  $scope.findFriends = function(query){
+    friendService.findFriends($scope.myProfile._id, query)
+  .then(function(){
+    return $scope.potentialFriends = [];
+    })
+  }
+
+   $scope.addFriend = function(friendId) {
+    friendService.addFriend($scope.myProfile._id, friendId)
+    .then(function(){
+      $scope.checkForProfile();
+  })
+  }
+
+  $scope.removeFriend = function(friendId) {
+    friendService.removeFriend($scope.myProfile._id, friendId)
+    .then(function(){
+      $scope.checkForProfile();
+  })
+  }
+
 });
--- profileService.js
+++ profileService.js
@@ -1,34 +1,34 @@
-angular.module("devMtIn")
-.service("profileService", function($http) {
+angular.module('devMtIn')
+.service('profileService', function($http) {
 
-	var baseUrl = "http://connections.devmounta.in/";
+var baseUrl = 'http://connections.devmounta.in/';
 	
 this.saveProfile = function(profile) {
-  return $http({
-    method: "POST"
+  return $http({ // Requests that your profile be added to the database
+    method: 'POST'
   , url: baseUrl + 'api/profiles/'
-  , data: profile 
-    })
-  .then(function(profileResponse) {
-  localStorage.setItem('profileId', JSON.stringify({ profileId: profileResponse.data._id }));
-  console.log(profileResponse);
+  , data: profile
+  })
+  .then(function(profileResponse) { // What to do after a response comes back from the server.
+  localStorage.setItem('profileId', JSON.stringify({ profileId: profileResponse.data._id })); // Save our unique _id to local storage
   })
   .catch(function(err) {
     console.error(err);
   });
-} 
-
-this.checkForProfile = function(profileId) {
-  return $http({
-    method: 'GET'
-  , url: baseUrl + 'api/profiles/' + profileId
-  });
 }
+
 this.deleteProfile = function() {
   var profileId = JSON.parse(localStorage.getItem('profileId')).profileId;
 
   return $http({
     method: 'DELETE'
+  , url: baseUrl + 'api/profiles/' + profileId
+  });
+}
+
+this.checkForProfile = function(profileId) {
+  return $http({
+    method: 'GET'
   , url: baseUrl + 'api/profiles/' + profileId
   });
 }
